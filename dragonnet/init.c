da776eb101354d0cf62e9c627a470c51a966f655
Elias Fleckenstein <eliasfleckenstein@web.de>
2022-04-25 18:26:28 +0200

    Add winsock initializer

---
+++ dragonnet/init.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "init.h"
+#include "sock.h"
+
+void dragonnet_init()
+{
+#ifdef _WIN32
+	printf("[info] initializing winsock\n");
+
+	WSADATA wsa_data;
+	if (WSAStartup(0x202, &wsa_data) != 0) {
+		fprintf(stderr, "[error] failed to initialize winsock\n");
+		abort();
+	}
+#endif
+}
+
+void dragonnet_deinit()
+{
+#ifdef _WIN32
+	WSACleanup();
+#endif
+}
---
+++ dragonnet/init.h
@@ -0,0 +1,7 @@
+#ifndef _DRAGONNET_INIT_H_
+#define _DRAGONNET_INIT_H_
+
+void dragonnet_init();
+void dragonnet_deinit();
+
+#endif // _DRAGONNET_INIT_H_
