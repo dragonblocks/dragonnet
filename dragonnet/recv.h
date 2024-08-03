b1b66bdeeb3e2322d0284a91032c8af0cf8c37fc
Lizzy Fleckenstein <lizzy@vlhl.dev>
2024-08-03 06:38:35 +0200

    only use weak attribute on non-windows

--- dragonnet/recv.h
+++ dragonnet/recv.h
@@ -3,15 +3,23 @@
 
 #include "peer.h"
 
+#ifdef WIN32
+#define WEAK
+#else
+#define WEAK __attribute__((weak))
+#endif
+
 typedef struct {
 	size_t siz;
 	bool (*deserialize)(DragonnetPeer *, void *);
 	void (*free)(void *);
 } DragonnetType;
 
-extern DragonnetTypeId dragonnet_num_types __attribute__((weak));
-extern DragonnetType dragonnet_types[] __attribute__((weak));
+extern DragonnetTypeId dragonnet_num_types WEAK;
+extern DragonnetType dragonnet_types[] WEAK;
 
 bool dragonnet_recv_raw(DragonnetPeer *p, void *buf, size_t n);
+
+#undef WEAK
 
 #endif
